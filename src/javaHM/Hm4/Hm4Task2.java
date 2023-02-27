81aa0d26aaabcac5136fe18b69787b555102c721
HadiakV <jekoird@gmail.com>
2023-02-27 09:39:10 +0100

    4

--- src/javaHM/Hm4/Hm4Task2.java
+++ src/javaHM/Hm4/Hm4Task2.java
@@ -10,14 +10,17 @@ public class Hm4Task2 {
     public static void main(String[] args) {
         int[] arr1 = {1, 5, 7, 1};
         int K1 = 6;
-        System.out.println(countPairs(arr1, K1)); // 2
+        int numPairs = countPairs(arr1, K1);
+        int numPossiblePairs = arr1.length * (arr1.length - 1) / 2;
+        System.out.println("Number of pairs with sum " + K1 + " in array " + ": " + numPairs);
+        System.out.println("Total number of possible pairs in array " + ": " + numPossiblePairs);
 
         int[] arr = {1, 1, 1, 1};
         int K = 2;
-        int numPairs = countPairs(arr, K);
-        int numPossiblePairs = arr.length * (arr.length - 1) / 2;
-        System.out.println("Number of pairs with sum " + K + " in array " + ": " + numPairs);
-        System.out.println("Total number of possible pairs in array " + ": " + numPossiblePairs);
+        int numPairs1 = countPairs(arr, K);
+        int numPossiblePairs1 = arr.length * (arr.length - 1) / 2;
+        System.out.println("Number of pairs with sum " + K + " in array " + ": " + numPairs1);
+        System.out.println("Total number of possible pairs in array " + ": " + numPossiblePairs1);
     }
 }
 
